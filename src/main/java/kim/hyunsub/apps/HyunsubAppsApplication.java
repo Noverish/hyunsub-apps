2168970c1b70d38645a28b70caf0cd67db5916b0
Hyunsub Kim <embrapers263@gmail.com>
2022-06-05 19:07:58 +0900

    init

---
+++ src/main/java/kim/hyunsub/apps/HyunsubAppsApplication.java
@@ -0,0 +1,13 @@
+package kim.hyunsub.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyunsubAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyunsubAppsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kim/hyunsub/apps/HyunsubAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.hyunsub.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyunsubAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
