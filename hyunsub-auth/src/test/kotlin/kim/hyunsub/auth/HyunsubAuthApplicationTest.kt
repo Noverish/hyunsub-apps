d4e2b8f78d2ebe95f87a982ac310f4ab5807aa83
Hyunsub Kim <embrapers263@gmail.com>
2022-06-05 19:22:07 +0900

    Multi Module

---
+++ hyunsub-auth/src/test/kotlin/kim/hyunsub/auth/HyunsubAuthApplicationTest.kt
@@ -0,0 +1,11 @@
+package kim.hyunsub.auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HyunsubAuthApplicationTest {
+	@Test
+	fun test() {
+	}
+}
