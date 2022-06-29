c27fa170a0df76f3f6a8bbce95ff75c9226d7ecc
Hyunsub Kim <embrapers263@gmail.com>
2022-06-30 00:49:20 +0900

    Using RequestHeader annotation in auth controller

--- hyunsub-auth/src/main/kotlin/kim/hyunsub/auth/controller/AuthController.kt
+++ hyunsub-auth/src/main/kotlin/kim/hyunsub/auth/controller/AuthController.kt
@@ -11,6 +11,7 @@ import kim.hyunsub.auth.service.JwtService
 import kim.hyunsub.util.log.Log
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.util.WebUtils
@@ -27,18 +28,21 @@ class AuthController(private val jwtService: JwtService) {
 	private val mapper = jacksonObjectMapper()
 
 	@GetMapping("")
-	fun auth(request: HttpServletRequest, response: HttpServletResponse) {
-		val originalIP = request.getHeader("X-Original-IP")
-		val originalUrl = request.getHeader("X-Original-URL")
+	fun auth(
+		request: HttpServletRequest,
+		response: HttpServletResponse,
+		@RequestHeader("X-Original-URL") originalUrl: String,
+		@RequestHeader("X-Original-IP") originalIp: String,
+	) {
 		val decodedUrl = URLDecoder.decode(originalUrl, StandardCharsets.UTF_8.toString()) // 로깅용
 
 		try {
 			val payload = parseJwt(request)
-			log.info("[Auth Success] payload={}, ip={}, url={}", payload, originalIP, decodedUrl)
+			log.info("[Auth Success] payload={}, ip={}, url={}", payload, originalIp, decodedUrl)
 			response.status = HttpStatus.OK.value()
 			response.setHeader(AppConstants.AUTH_HEADER_NAME, mapper.writeValueAsString(payload))
 		} catch (e: ErrorCodeException) {
-			log.info("[Auth Failed] {}: ip={}, url={}", e.message, originalIP, decodedUrl)
+			log.info("[Auth Failed] {}: ip={}, url={}", e.message, originalIp, decodedUrl)
 			val redirectUrl = "https://${AppConstants.AUTH_DOMAIN}/login?url=$originalUrl"
 			response.setHeader("X-Redirect-URL", redirectUrl)
 			response.status = HttpStatus.UNAUTHORIZED.value()
@@ -46,15 +50,18 @@ class AuthController(private val jwtService: JwtService) {
 	}
 
 	@GetMapping("/file")
-	fun authFile(request: HttpServletRequest, response: HttpServletResponse) {
-		val originalIP = request.getHeader("X-Original-IP")
-		val originalUrl = request.getHeader("X-Original-URL")
+	fun authFile(
+		request: HttpServletRequest,
+		response: HttpServletResponse,
+		@RequestHeader("X-Original-URL") originalUrl: String,
+		@RequestHeader("X-Original-IP") originalIp: String,
+	) {
 		val decodedUrl = URLDecoder.decode(originalUrl, StandardCharsets.UTF_8.toString()) // 로깅용
 
 		val payload = try {
 			 parseJwt(request)
 		} catch (e: ErrorCodeException) {
-			log.info("[AuthFile Failed] {}: ip={}, url={}", e.message, originalIP, decodedUrl)
+			log.info("[AuthFile Failed] {}: ip={}, url={}", e.message, originalIp, decodedUrl)
 			response.status = HttpStatus.UNAUTHORIZED.value()
 			return
 		}
@@ -62,11 +69,11 @@ class AuthController(private val jwtService: JwtService) {
 		val path = URL(decodedUrl).path
 		val allowed = payload.paths.any { path.startsWith(it) }
 		if (allowed) {
-			log.info("[AuthFile Success] payload={}, ip={}, url={}", payload, originalIP, decodedUrl)
+			log.info("[AuthFile Success] payload={}, ip={}, url={}", payload, originalIp, decodedUrl)
 			response.status = HttpStatus.OK.value()
 			response.setHeader(AppConstants.AUTH_HEADER_NAME, mapper.writeValueAsString(payload))
 		} else {
-			log.info("[AuthFile Failed] Forbidden: payload={}, ip={}, url={}", payload, originalIP, decodedUrl)
+			log.info("[AuthFile Failed] Forbidden: payload={}, ip={}, url={}", payload, originalIp, decodedUrl)
 			response.status = HttpStatus.FORBIDDEN.value()
 		}
 	}
