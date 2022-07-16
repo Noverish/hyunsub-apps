84e288159db1c2221b2febf9f4190619e5411121
Hyunsub Kim <embrapers263@gmail.com>
2022-07-17 02:05:32 +0900

    Change Token Cookie Name

--- hyunsub-common/src/main/kotlin/kim/hyunsub/common/web/config/WebConstants.kt
+++ hyunsub-common/src/main/kotlin/kim/hyunsub/common/web/config/WebConstants.kt
@@ -1,6 +1,6 @@
 package kim.hyunsub.common.web.config
 
 object WebConstants {
-	const val TOKEN_COOKIE_NAME = "HYUNSUB_JWT"
+	const val TOKEN_COOKIE_NAME = "HYUNSUB_TOKEN"
 	const val USER_AUTH_HEADER = "X-Hyunsub-Auth"
 }
