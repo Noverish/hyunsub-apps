0ac343fd8e00c906c56b3db5e463a433a1de33a6
Hyunsub Kim <embrapers263@gmail.com>
2022-09-04 17:46:45 +0900

    HyunsubVideoApplicationTest

---
+++ hyunsub-video/src/test/kotlin/kim/hyunsub/video/HyunsubVideoApplicationTest.kt
@@ -0,0 +1,11 @@
+package kim.hyunsub.video
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HyunsubVideoApplicationTest {
+	@Test
+	fun contextLoads() {
+	}
+}
