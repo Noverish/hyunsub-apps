1296fd7908dbfb4a29cd465ed590f3b2d07f0bd8
Hyunsub Kim <embrapers263@gmail.com>
2023-02-25 11:10:21 +0900

    Show videos in home randomly

--- hyunsub-video/src/main/kotlin/kim/hyunsub/video/controller/VideoHomeController.kt
+++ hyunsub-video/src/main/kotlin/kim/hyunsub/video/controller/VideoHomeController.kt
@@ -9,6 +9,7 @@ import kim.hyunsub.video.service.VideoCategoryService
 import org.springframework.data.domain.PageRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -21,13 +22,16 @@ class VideoHomeController(
 	@GetMapping("")
 	fun home(
 		userAuth: UserAuth,
+		@RequestParam seed: Int?,
 	): VideoHomeResult {
 		val categories = videoCategoryService.getAvailableCategories(userAuth)
+
+		val randomSeed = seed ?: System.currentTimeMillis().toInt()
 
 		val recents = categories.map { category ->
 			VideoHomeRecent(
 				category = category.toDto(),
-				list = videoEntryRepository.findByCategoryOrderByRegDtDesc(category.name, PageRequest.of(0, 12))
+				list = videoEntryRepository.findByCategoryOrderByRand(category.name, randomSeed, PageRequest.of(0, 12))
 					.map { it.toDto() }
 			)
 		}
