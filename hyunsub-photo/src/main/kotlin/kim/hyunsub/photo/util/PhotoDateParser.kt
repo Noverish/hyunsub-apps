c7edd7b21112dc193cfc15501b40dd842dcb6a9a
Hyunsub Kim <embrapers263@gmail.com>
2023-03-25 01:35:16 +0900

    more detail date parse

--- hyunsub-photo/src/main/kotlin/kim/hyunsub/photo/util/PhotoDateParser.kt
+++ hyunsub-photo/src/main/kotlin/kim/hyunsub/photo/util/PhotoDateParser.kt
@@ -6,13 +6,17 @@ import java.time.Instant
 import java.time.LocalDateTime
 import java.time.OffsetDateTime
 import java.time.ZoneId
+import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
 
 object PhotoDateParser {
 	private val log = KotlinLogging.logger { }
-	private val format1 = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss.SSSXXX")
-	private val format2 = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ssXXX")
+	private val odtFormat1 = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss.SSSXXX")
+	private val odtFormat2 = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ssXXX")
+	private val ldtFormat1 = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss.SSSS")
+	private val ldtFormat2 = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss")
+	private val ldtFormat3 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 	private val nameFormatMap = mapOf(
 		"20\\d{2}-\\d{2}-\\d{2}-\\d{2}-\\d{2}-\\d{2}" to "yyyy-MM-dd-HH-mm-ss",
 		"20\\d{6}_\\d{6}_\\d{3}" to "yyyyMMdd_HHmmss_SSS",
@@ -42,7 +46,7 @@ object PhotoDateParser {
 			return dateFromName
 		}
 
-		val modifyDate = parseFromExif(exif, fileName, "FileModifyDate")
+		val modifyDate = parseFromExifAsOdt(exif, fileName, "FileModifyDate")
 		if (modifyDate != null) {
 			return modifyDate
 		}
@@ -52,18 +56,30 @@ object PhotoDateParser {
 	}
 
 	private fun parseImage(exif: JsonNode, fileName: String): OffsetDateTime? {
-		return parseFromExif(exif, fileName, "SubSecDateTimeOriginal")
-			?: parseFromExif(exif, fileName, "DateTimeOriginal")
+		return parseFromExifAsOdt(exif, fileName, "SubSecDateTimeOriginal")
+			?: parseFromExifAsOdt(exif, fileName, "DateTimeOriginal")
+			?: parseFromExifAsOdt(exif, fileName, "GPSDateTime")
+			?: parseFromExifAsOdt(exif, fileName, "TimeStamp")
+			?: parseFromExifAsOdt(exif, fileName, "ModifyDate")
+
+			?: parseFromExifAsLdt(exif, fileName, "SubSecDateTimeOriginal")
+			?: parseFromExifAsLdt(exif, fileName, "DateTimeOriginal")
+			?: parseFromExifAsLdt(exif, fileName, "GPSDateTime")
+			?: parseFromExifAsLdt(exif, fileName, "TimeStamp")
+			?: parseFromExifAsLdt(exif, fileName, "ModifyDate")
 	}
 
 	private fun parseVideo(exif: JsonNode, fileName: String): OffsetDateTime? {
-		return parseFromExif(exif, fileName, "CreationDate")
+		return parseFromExifAsOdt(exif, fileName, "CreationDate")
 	}
 
-	private fun parseFromExif(exif: JsonNode, fileName: String, field: String): OffsetDateTime? {
+	private fun parseFromExifAsOdt(exif: JsonNode, fileName: String, field: String): OffsetDateTime? {
 		val str = exif[field]?.textValue() ?: return null
+		if (str.startsWith("0000")) {
+			return null
+		}
 
-		for (format in listOf(format1, format2)) {
+		for (format in listOf(odtFormat1, odtFormat2)) {
 			try {
 				val result = OffsetDateTime.parse(str, format)
 				log.debug { "[Parse Photo Date] $fileName: From $field - $str" }
@@ -71,6 +87,32 @@ object PhotoDateParser {
 			} catch (_: DateTimeParseException) {
 			}
 		}
+
+		return null
+	}
+
+	private fun parseFromExifAsLdt(exif: JsonNode, fileName: String, field: String): OffsetDateTime? {
+		val str = exif[field]?.textValue() ?: return null
+		if (str.startsWith("0000")) {
+			return null
+		}
+
+		val offsetStr = listOfNotNull(
+			exif["OffsetTime"]?.asText(),
+			exif["OffsetTimeOriginal"]?.asText(),
+			exif["OffsetTimeDigitized"]?.asText(),
+		).firstOrNull()
+		val offset = offsetStr?.let { ZoneOffset.of(it) } ?: ZoneId.systemDefault()
+
+		for (format in listOf(ldtFormat1, ldtFormat2, ldtFormat3)) {
+			try {
+				val result = LocalDateTime.parse(str, format).atZone(offset).toOffsetDateTime()
+				log.debug { "[Parse Photo Date] $fileName: From $field - $str $offsetStr" }
+				return result
+			} catch (_: DateTimeParseException) {
+			}
+		}
+
 		return null
 	}
 
