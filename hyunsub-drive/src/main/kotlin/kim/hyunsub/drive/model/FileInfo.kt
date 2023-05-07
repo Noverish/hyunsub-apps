7cfecb8fe615924d808421897871229200da192d
Hyunsub Kim <embrapers263@gmail.com>
2023-05-07 18:40:34 +0900

    [drive] FileInfo.size to number

--- hyunsub-drive/src/main/kotlin/kim/hyunsub/drive/model/FileInfo.kt
+++ hyunsub-drive/src/main/kotlin/kim/hyunsub/drive/model/FileInfo.kt
@@ -1,20 +1,19 @@
 package kim.hyunsub.drive.model
 
 import kim.hyunsub.common.api.model.FileStat
-import kim.hyunsub.common.util.getHumanReadableSize
 import java.time.LocalDateTime
 import kotlin.io.path.Path
 import kotlin.io.path.name
 
 data class FileInfo(
 	val name: String,
-	val size: String,
+	val size: Long,
 	val date: LocalDateTime,
 	val isDir: Boolean,
 ) {
 	constructor(stat: FileStat) : this(
 		name = Path(stat.path).name,
-		size = if (stat.isDir == true) "" else getHumanReadableSize(stat.size),
+		size = if (stat.isDir == true) 0 else stat.size,
 		date = stat.mDate,
 		isDir = stat.isDir == true,
 	)
