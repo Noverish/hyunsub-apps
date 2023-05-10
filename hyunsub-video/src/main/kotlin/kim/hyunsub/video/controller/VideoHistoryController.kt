86f1645f424e7b787c9ba1a5ec436959d6f5630c
Hyunsub Kim <embrapers263@gmail.com>
2023-05-10 21:55:20 +0900

    [video] changed history socket path

--- hyunsub-video/src/main/kotlin/kim/hyunsub/video/controller/VideoHistoryController.kt
+++ hyunsub-video/src/main/kotlin/kim/hyunsub/video/controller/VideoHistoryController.kt
@@ -1,21 +1,28 @@
 package kim.hyunsub.video.controller
 
 import kim.hyunsub.common.model.RestApiPageResult
+import kim.hyunsub.common.web.config.userAuth
 import kim.hyunsub.common.web.model.UserAuth
+import kim.hyunsub.video.model.dto.VideoHistoryCreateParams
 import kim.hyunsub.video.repository.VideoHistoryRepository
+import kim.hyunsub.video.repository.entity.VideoHistory
 import kim.hyunsub.video.repository.entity.VideoMyHistory
+import mu.KotlinLogging
 import org.springframework.data.domain.PageRequest
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
-@RequestMapping("/api/v1/histories")
 class VideoHistoryController(
 	private val videoHistoryRepository: VideoHistoryRepository,
 ) {
-	@GetMapping("")
+	private val log = KotlinLogging.logger { }
+
+	@GetMapping("/api/v1/histories")
 	fun list(
 		user: UserAuth,
 		@RequestParam(required = false, defaultValue = "0") p: Int,
@@ -33,5 +40,25 @@ class VideoHistoryController(
 			pageSize = ps,
 			data = list
 		)
+	}
+
+	@MessageMapping("/v1/histories")
+	fun videoHistory(params: VideoHistoryCreateParams, accessor: SimpMessageHeaderAccessor) {
+		if (params.time == 0) {
+			return
+		}
+
+		val userAuth = accessor.userAuth
+
+		val history = VideoHistory(
+			userId = userAuth.idNo,
+			videoId = params.videoId,
+			time = params.time,
+			date = LocalDateTime.now(),
+		)
+
+		log.debug { "[Create Video History] $history" }
+
+		videoHistoryRepository.save(history)
 	}
 }
