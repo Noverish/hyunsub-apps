6b368c6c657ca1977cb45dc83bc8a76cd46c04ae
Hyunsub Kim <embrapers263@gmail.com>
2023-07-16 23:13:43 +0900

    [photo] Added AlbumOwnerController

---
+++ hyunsub-photo/src/main/kotlin/kim/hyunsub/photo/controller/AlbumOwnerController.kt
@@ -0,0 +1,27 @@
+package kim.hyunsub.photo.controller
+
+import kim.hyunsub.common.web.model.SimpleResponse2
+import kim.hyunsub.photo.model.dto.AlbumOwnerCreateParams
+import kim.hyunsub.photo.repository.AlbumOwnerRepository
+import kim.hyunsub.photo.repository.entity.AlbumOwner
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v2/albums/{albumId}/owners")
+class AlbumOwnerController(
+	private val albumOwnerRepository: AlbumOwnerRepository,
+) {
+	@PostMapping("")
+	fun register(
+		@PathVariable albumId: String,
+		@RequestBody params: AlbumOwnerCreateParams,
+	): SimpleResponse2 {
+		val albumOwner = AlbumOwner(albumId, params.userId, false)
+		albumOwnerRepository.save(albumOwner)
+		return SimpleResponse2()
+	}
+}
---
+++ hyunsub-photo/src/main/kotlin/kim/hyunsub/photo/model/dto/AlbumOwnerCreateParams.kt
@@ -0,0 +1,5 @@
+package kim.hyunsub.photo.model.dto
+
+data class AlbumOwnerCreateParams(
+	val userId: String,
+)
