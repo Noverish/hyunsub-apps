4d371b78e29914b7296413084a6825e960183590
Hyunsub Kim <embrapers263@gmail.com>
2023-08-26 23:07:39 +0900

    [comic] thumbnail

--- hyunsub-comic/src/main/kotlin/kim/hyunsub/comic/model/ApiComicPreview.kt
+++ hyunsub-comic/src/main/kotlin/kim/hyunsub/comic/model/ApiComicPreview.kt
@@ -1,6 +1,13 @@
 package kim.hyunsub.comic.model
 
+import kim.hyunsub.comic.config.ComicConstants
+import kim.hyunsub.common.fs.FsPathConverter
+import kotlin.io.path.Path
+
 data class ApiComicPreview(
 	val id: String,
 	val title: String,
-)
+) {
+	val thumbnail: String
+		get() = FsPathConverter.convertToUrl(Path(ComicConstants.BASE_PATH, title, "thumbnail.jpg").toString())
+}
