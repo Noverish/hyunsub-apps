7bebfd7ddd0324c786c0fbd4408c886b65c556ec
Hyunsub Kim <embrapers263@gmail.com>
2024-02-24 14:51:44 +0900

    [photo] mapper xml not use asterisk

--- hyunsub-photo/src/main/kotlin/kim/hyunsub/photo/repository/entity/AlbumOwner.kt
+++ hyunsub-photo/src/main/kotlin/kim/hyunsub/photo/repository/entity/AlbumOwner.kt
@@ -1,7 +1,10 @@
 package kim.hyunsub.photo.repository.entity
 
+import java.time.LocalDateTime
+
 data class AlbumOwner(
 	val albumId: String,
 	val userId: String,
 	val owner: Boolean,
+	val regDt: LocalDateTime = LocalDateTime.now(),
 )
--- hyunsub-photo/src/main/kotlin/kim/hyunsub/photo/repository/entity/AlbumPhoto.kt
+++ hyunsub-photo/src/main/kotlin/kim/hyunsub/photo/repository/entity/AlbumPhoto.kt
@@ -1,7 +1,10 @@
 package kim.hyunsub.photo.repository.entity
 
+import java.time.LocalDateTime
+
 data class AlbumPhoto(
 	val albumId: String,
 	val photoId: String,
 	val userId: String,
+	val regDt: LocalDateTime = LocalDateTime.now(),
 )
